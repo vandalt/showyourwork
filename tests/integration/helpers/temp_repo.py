5a39081b53211981beb17e5c844677cc86f34726
Thomas Vandal <thomas.vandal@umontreal.ca>
2026-02-27 04:13:34 -0500

    Allow test code to run on CI when `run_cache_rules_on_ci` is true (#655)
    * Add configuration option to run tests with github user instead of showyourwork org
    
    * Update Integration tests docs to mention the possiblity to run them locally
    
    * Enforce "no run on ci" only when config does not override it

--- tests/integration/helpers/temp_repo.py
+++ tests/integration/helpers/temp_repo.py
@@ -485,8 +485,9 @@ class ShowyourworkRepositoryActions:
                         "import numpy as np",
                         "import paths",
                         "import os",
-                        "if os.getenv('CI', 'false') == 'true' or "
-                        "os.getenv('SYW_NO_RUN', 'false') == 'true':",
+                        "no_run_ci = os.getenv('CI', 'false') == 'true' and "
+                        "not snakemake.config['run_cache_rules_on_ci']",
+                        "if no_run_ci or os.getenv('SYW_NO_RUN', 'false') == 'true':",
                         "    raise Exception('Output should have been downloaded from "
                         "Zenodo.')",
                         f"np.random.seed({seed})",
